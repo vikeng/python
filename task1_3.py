33b42dd0ea01c95bb899c8ec620e2ab4cdb8ab88
vikeng <vikengui@gmail.com>
2015-04-26 21:58:36 +0800

    Tasks 4 and 5

--- task1_3.py
+++ task1_3.py
@@ -34,16 +34,54 @@ def sort_end(str_list):
   return str_list3
 
 
+def insert_str(list,str):
+  f = 1
+  for l in list:
+    if str<l:
+      k = list.index(l)
+      list.insert(k,str)
+      f = 0 
+      break
+  if f:
+    list.append(str)
+      
+  return list
+
+def search_insert(list1,list2,x):
+  count = list1.count(x)
+  for i in xrange(count):
+    k = list1.index(x)
+    list1[k] = str(x)+'!'
+    list2.insert(k,x)
+  return list2
+
 a = [1,5,6,5,3,3,3,2,23,4,55,2]
+print "N1"
+print a
 print repeat_list(a)
 
+
 a = [1,'Hello',[1,4,7],'Yes',5,7,9+8j,['ya.ru','google.ru'],{1:'python',2:'php',3:'awk'}]
+print "\nN2"
+print a
 print_type_stat(a)
 
 
 a = ['Hello','yandex','gmail','python','RU']
+print "\nN3"
+print a
 print sort_end(a)
 
 
-  
+list = ['asdf','bbb','ddd']
+print "\nN4"
+print list
+print insert_str(list,'ccc')
 
+list1 = [1,2,3,4,3,6,7,8,3]
+list2 = ['a','b','c','d','e']
+
+print "\nN5"
+print list1
+print list2
+print search_insert(list1,list2,3)
