049bf8415106a6bd00d0074230610168d615b6d0
vikeng <vikengui@gmail.com>
2015-05-06 13:01:54 +0800

    Добавлена 6-я задача

--- task1_4.py
+++ task1_4.py
@@ -14,12 +14,13 @@ def dict_merge(d1,d2):
     del d1[s]
     del d2[s]
   return dict(d1.items()+d2.items())
-  # return sd
-  
 
 
+def swap_dict(d):
+  return dict([value,key] for key,value in d.iteritems())
+
   
-print "Task 4_1"
+print "Task 4_3"
 d = {'a':1,'b':2,'c':3}
 str = 'b'
 print d
@@ -27,13 +28,22 @@ print 'str = ' + str
 print dict_str_None(d,str)
 
 
-print "Task 4_2"
+print "Task 4_4"
 d1 = {'a':1,'b':2,'c':3}
 d2 = {'e':4,'b':4,'t':7}
 print d1
 print d2
 
 print dict_merge(d1,d2)
+
+
+
+print "Task 4_6"
+d = {'a':1,'b':2,'c':3}
+print d
+
+print swap_dict(d)
+
 
 
 
